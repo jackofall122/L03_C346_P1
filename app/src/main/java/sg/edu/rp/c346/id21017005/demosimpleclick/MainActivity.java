8be2dfbd27691ccb65c77dc23789fd5f44864d64
jackofall122 <21017005@myrp.edu.sg>
2023-05-04 10:08:30 +0800

    hi

--- app/src/main/java/sg/edu/rp/c346/id21017005/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21017005/demosimpleclick/MainActivity.java
@@ -16,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvDisplay=findViewById(R.id.textViewDisplay);
+        btnDisplay=findViewById(R.id.buttonDisplay);
+        etInput=findViewById(R.id.et1);
     }
 }
